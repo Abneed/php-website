11668c16494e8513406d4bf9eda511a8bc932d16
Abneed <memoboy262@hotmail.com>
2018-11-30 05:10:23 -0600

    Estructura formulario de registrar usuario

---
+++ signup.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registro de usuario | PHP Website</title>
+    <link rel="stylesheet" href="basic-layout.css">
+</head>
+<body>
+    <div class="navbar">
+        <h2>Registro de usuario</h2>
+        <div class="button">
+            <a href="signin.php">Iniciar sesión</a>
+        </div>
+    </div>
+    <form action="" method="post">
+        <label for="usuario">Usuario:</label>
+        <input type="text" name="usuario" id="usuario"><br>
+
+        <label for="nombre">Nombre completo:</label>
+        <input type="text" name="nombre" id="nombre"><br>
+
+        <label for="password">Contraseña:</label>
+        <input type="password" name="password" id="password"><br>
+
+        <label for="confirm_password">Confirmar contraseña:</label>
+        <input type="password" name="conf_password" id="conf_password"><br>
+
+        <label for="tipo_usuario">Tipo de usuario:</label>
+        <input type="text" name="tipo_usuario" id="tipo_usuario"><br>
+
+        <input type="submit" value="Registrar">
+        <input type="reset" value="Limpiar">
+    </form>
+</body>
+</html>
