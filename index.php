03a7215c6b628e4440e3d21b1abc10801209e9e4
Abneed <memoboy262@hotmail.com>
2018-11-30 03:58:24 -0600

    Haz un boquejo de la barra de navegacion
    Diseña un boquejo de la barra de navegacion, con los hipervinculos para
    iniciar sesion y registro de usuarios.

--- index.php
+++ index.php
@@ -5,9 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inicio | PHP Website</title>
+    <style>
+        .navbar
+        {
+            border: 6px solid black;
+            border-radius: 12px;
+            padding: 6px;
+        }
+        div.button
+        {
+            text-align: right;
+        }
+    </style>
 </head>
 <body>
-    <h2>Bienvenido ha PHP Website</h2>
+    <div class="navbar">
+        <h2>Bienvenido ha PHP Website</h2>
+        <div class="button">
+            <a href="signin.php">Iniciar sesión</a>
+        </div>
+        <div class="button">
+            <a href="signup.php">Registrarse</a>
+        </div>
+    </div>
     <p>
         Proyecto final para la asignatura "Programacion Web".
     </p>
